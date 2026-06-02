710c1acd8dfd1c799fd87b651b7add0f1cbb0c9a
Jayden Chun <jayden.h.chun@gmail.com>
2026-06-02 00:11:43 -0500

    fixed the doubling issue in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,22 @@
 export function loadNavBar() {
+  // Prevent duplicate navbar injection
+  if (document.querySelector('nav.navbar')) {
+    return;
+  }
+
   const navHTML = `
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
+        <a class="navbar-brand" href="/">KSJ Swim</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav" style = "justify-content: space-between; width: 80%; margin: 0 auto">
-            <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+          <div class="navbar-nav" style="justify-content: space-between; width: 80%; margin: 0 auto">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
             <a class="nav-link" href="/src/pages/instructors.html">Instructors</a>
-            <a class="nav-link" href="404.html">Pricing</a>
-            <a class="nav-link" href="404.html">Sign-Ups</a>
+            <a class="nav-link" href="#pricing">Pricing</a>
+            <a class="nav-link" href="#signup">Sign-Ups</a>
           </div>
         </div>
       </div>
