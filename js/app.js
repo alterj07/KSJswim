7d77860c8c24372f8e7a78b8a5e19706deb2cbd0
Jayden Chun <jayden.h.chun@gmail.com>
2026-06-04 10:55:04 +0900

    feat: added the pricing components into the pricing page via js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ import { loadNavBar } from '../src/components/NavBar/NavBar.js';
 import { loadJoshCard } from '../src/components/CardTitle/Instructors/Josh.js';
 import { loadInstructor2Card } from '../src/components/CardTitle/Instructors/instructor2.js';
 import { loadInstructor3Card } from '../src/components/CardTitle/Instructors/instructor3.js';
-
+import { loadPricing1Card } from '../src/components/CardTitle/Pricing/pricing1.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('KSJ Swim app initialized');
@@ -42,4 +42,19 @@ if(document.getElementById('instructor2')) {
 if(document.getElementById('instructor3')) {
     const instructor3CardHTML = loadInstructor3Card();
     document.getElementById('instructor3').innerHTML = instructor3CardHTML;
+}
+
+if(document.getElementById('pricing1')) {
+    const pricing1CardHTML = loadPricing1Card();
+    document.getElementById('pricing1').innerHTML = pricing1CardHTML;
+}
+
+if(document.getElementById('pricing2')) {
+    const pricing2CardHTML = loadPricing1Card();
+    document.getElementById('pricing2').innerHTML = pricing2CardHTML;
+}
+
+if(document.getElementById('pricing3')) {
+    const pricing3CardHTML = loadPricing1Card();
+    document.getElementById('pricing3').innerHTML = pricing3CardHTML;
 }
