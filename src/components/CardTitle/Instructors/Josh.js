1473cf23b2d4f68be1b0eda032260d0583811db6
Jayden Chun <jayden.h.chun@gmail.com>
2026-06-08 23:13:51 +0900

    fix: text doesn't overflow in instructors card in instructors page

--- src/components/CardTitle/Instructors/Josh.js
+++ src/components/CardTitle/Instructors/Josh.js
@@ -1,9 +1,9 @@
 export function loadJoshCard() {
     const joshHTML = `
-    <div class="card" style="width: 25vw">
+    <div class="card" style="width: 25vw;">
         <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        <div class="card-body" style="width: 25vw;">
+            <p class="card-text" style="overflow: hidden">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
         </div>
     </div>
     `;
